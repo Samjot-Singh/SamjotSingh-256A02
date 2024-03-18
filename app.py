4303aa54b9283f5b975d3287ba44b0d499aa28a9
Samjot <samjotsingh04@gmail.com>
2024-03-18 00:46:43 -0700

    added edit and delete

--- app.py
+++ app.py
@@ -4,12 +4,13 @@ Assignment 2 - Web Server
 Samjot Singh
 Topic- Create a functional web server using either the Bottle or Flask python library
 
-Took some help from chatgpt 3.5 for syntax/runtime errors and reformattiing the code.
+Took some help from chatgpt 3.5 for syntax/runtime errors and reformatting the code.
 Used lab05 Flask server for reference.
 Used https://flask.palletsprojects.com/en/3.0.x/quickstart/ for reference.
 Used https://www.geeksforgeeks.org/how-to-run-a-flask-application/ for reference.
 Used https://www.geeksforgeeks.org/flask-creating-first-simple-application/ for reference.
 """
+
 from flask import Flask, render_template, request, redirect, session, url_for, flash
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, RadioField, SelectField, IntegerField, FloatField, DateField, SubmitField, HiddenField
@@ -104,26 +105,30 @@ def pizza():
     if 'email' not in session:
         flash('Please login to view this page.', 'danger')
         return redirect(url_for('login'))
+
     form = PizzaForm()
     init_data = load_init_data()
     form.type.choices = [(t, t) for t in init_data['type']]
     form.crust.choices = [(c, c) for c in init_data['crust']]
     form.size.choices = [(s, s) for s in init_data['size']]
-    if form.validate_on_submit():
-        pizza_data = load_pizza_data()
-        pizza_order = {
-            'id': len(pizza_data) + 1,
-            'type': form.type.data,
-            'crust': form.crust.data,
-            'size': form.size.data,
-            'quantity': form.quantity.data,
-            'price_per': form.price_per.data,
-            'order_date': form.order_date.data.strftime('%Y/%m/%d')
-        }
-        pizza_data.append(pizza_order)
-        write_pizza_data(pizza_data)
-        flash('Pizza order placed successfully!', 'success')
-        return redirect(url_for('index'))
+
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            pizza_data = load_pizza_data()
+            pizza_order = {
+                'id': len(pizza_data) + 1,
+                'type': form.type.data,
+                'crust': form.crust.data,
+                'size': form.size.data,
+                'quantity': form.quantity.data,
+                'price_per': form.price_per.data,
+                'order_date': form.order_date.data.strftime('%Y-%m-%d')
+            }
+            pizza_data.append(pizza_order)
+            write_pizza_data(pizza_data)
+            flash('Pizza order placed successfully!', 'success')
+            return redirect(url_for('index'))
+
     return render_template('pizza.html', form=form)
 
 @app.route('/logout')
@@ -133,5 +138,64 @@ def logout():
     flash('You have been logged out successfully.', 'success')
     return redirect(url_for('login'))
 
+@app.route('/confirm_delete/<int:order_id>', methods=['GET', 'POST'])
+def confirm_delete(order_id):
+    if 'email' not in session:
+        flash('Please login to view this page.', 'danger')
+        return redirect(url_for('login'))
+    
+    if request.method == 'POST':
+        pizza_data = load_pizza_data()
+        updated_pizza_data = [order for order in pizza_data if order['id'] != order_id]
+        write_pizza_data(updated_pizza_data)
+        flash('Order deleted successfully!', 'success')
+        return redirect(url_for('index'))
+    
+    pizza_data = load_pizza_data()
+    order_to_delete = [order for order in pizza_data if order['id'] == order_id][0]
+    return render_template('confirm_delete.html', order=order_to_delete)
+
+@app.route('/edit_order/<int:order_id>', methods=['GET', 'POST'])
+def edit_order(order_id):
+    if 'email' not in session:
+        flash('Please login to view this page.', 'danger')
+        return redirect(url_for('login'))
+    
+    form = PizzaForm()
+    init_data = load_init_data()
+    form.type.choices = [(t, t) for t in init_data['type']]
+    form.crust.choices = [(c, c) for c in init_data['crust']]
+    form.size.choices = [(s, s) for s in init_data['size']]
+    
+    pizza_data = load_pizza_data()
+    order_to_edit = [order for order in pizza_data if order['id'] == order_id][0]
+
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            pizza_data = load_pizza_data()
+            updated_orders = []
+            for order in pizza_data:
+                if order['id'] == order_id:
+                    order['type'] = form.type.data
+                    order['crust'] = form.crust.data
+                    order['size'] = form.size.data
+                    order['quantity'] = form.quantity.data
+                    order['price_per'] = form.price_per.data
+                    order['order_date'] = form.order_date.data.strftime('%Y-%m-%d')
+                updated_orders.append(order)
+            write_pizza_data(updated_orders)
+            flash('Order updated successfully!', 'success')
+            return redirect(url_for('index'))
+
+    form.type.data = order_to_edit['type']
+    form.crust.data = order_to_edit['crust']
+    form.size.data = order_to_edit['size']
+    form.quantity.data = order_to_edit['quantity']
+    form.price_per.data = order_to_edit['price_per']
+    form.order_date.data = order_to_edit['order_date']
+
+    return render_template('edit_order.html', form=form)
+
 if __name__ == '__main__':
     app.run(debug=True, port=8888)
+
